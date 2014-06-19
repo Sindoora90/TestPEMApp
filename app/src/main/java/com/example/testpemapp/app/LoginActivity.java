1faccb0a961e0d5eb633fe78811ef147301f1334
Nora <schuster_no@yahoo.de>
2014-06-19 23:50:06 +0200

    other files

--- app/src/main/java/com/example/testpemapp/app/LoginActivity.java
+++ app/src/main/java/com/example/testpemapp/app/LoginActivity.java
@@ -3,14 +3,17 @@ package com.example.testpemapp.app;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -25,6 +28,7 @@ public class LoginActivity extends Activity {
     EditText pw;
     EditText phone;
     EditText adr;
+    TextView description;
 
 
     @Override
@@ -32,6 +36,13 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+
+        description = (TextView) findViewById(R.id.logintextView);
+
+        // other font_family:
+        //Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/segoe-ui-1361529660.ttf");
+        //description.setTypeface(tf);
 
         Button b = (Button) findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
